9f946e7f009eef22478ef871b2d9b0e784ed5ce9
Thiago Vinicius <116599158+Thiago309@users.noreply.github.com>
2024-06-14 13:00:29 +0000

    Correcao do exercicio 11

--- Exercicios/Comandos_Sequenciais/Questao11.c
+++ Exercicios/Comandos_Sequenciais/Questao11.c
@@ -11,7 +11,7 @@ apresentar a quantidade de c ´ edulas e moedas para compor o troco do cliente.
 int main(){
 
     float valorCompra, valorPago, troco;
-    int notaCem, notaCinquen, notaVinte, notaDez, resto;
+    int notaDuz, notaDois, notaCinco, notaCem, notaCinquen, notaVinte, notaDez, resto;
 
     printf("Informe o valor da compra do cliente: ");
     scanf("%f", &valorCompra);
@@ -21,10 +21,50 @@ int main(){
     troco = valorPago - valorCompra;
     resto = troco;
 
-    notaCem =  resto % 100;
-    notaCinquen = resto % 50;
-    notaVinte = resto % 20;
-    notaDez = resto % 10;
+    notaDuz =  troco / 200;
+    resto = resto - (notaDuz * 200);
+
+    notaCem =  troco / 100;
+    resto = resto - (notaCem * 100);
+
+    notaCinquen = resto / 50;
+    resto = resto - (notaCinquen * 50);
+
+    notaVinte = resto / 20;
+    resto = resto - (notaVinte * 20);
+
+    notaDez = resto / 10;
+    resto = resto - (notaDez * 10);
+
+    notaCinco = resto / 5;
+    resto = resto - (notaCinco * 5);
+
+    notaDois = resto / 2;
+    resto = resto - (notaDois * 2);
+
+    // Convertendo o resto para centavos...
+
+    notaDuz = resto / 200;
+    resto = resto % 200;
+
+    notaCem = resto / 100;
+    resto = resto % 100;
+
+    notaCinquen = resto / 50;
+    resto = resto % 50;
+
+    notaVinte = resto / 20;
+    resto = resto % 20;
+
+    notaDez = resto / 10;
+    resto = resto % 10;
+
+    notaCinco = resto / 5;
+    resto = resto % 5;
+
+    notaDois = resto / 2;
+    resto = resto % 2;
+    
     
     printf("Você deve devolver ao cliente R$%.2f . \n", troco);
     printf("R$100.00: %d Notas | R$50.00: %d Notas | R$20.00: %d Notas | R$10.00: %d Notas |\n", notaCem, notaCinquen, notaVinte, notaDez);
